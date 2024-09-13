ea4d7588c1820ace5c1d60e56c2effdef287cf35
natersland <natersland@gmail.com>
2024-09-13 07:43:09 +0700

    feat: add chart in detail page

--- lib/src/features/stock/stock_detail/stock_detail_page.dart
+++ lib/src/features/stock/stock_detail/stock_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stock_ranking_project/src/domain/entities/stock/stock_ranking_entity.dart';
+import 'package:stock_ranking_project/src/features/stock/stock_detail/widgets/stock_line_chart.dart';
 
 class StockDetailPage extends StatelessWidget {
   final StockRankingEntity stock;
@@ -8,41 +9,49 @@ class StockDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final graphs = stock.graphs;
     return Scaffold(
       appBar: AppBar(
         title: Text(stock.symbol),
       ),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            _buildText(
               'Company: ${stock.name}',
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
             ),
             const SizedBox(height: 16),
-            Text(
-              'Jitta Score: ${stock.jittaScore}',
-              style: TextStyle(fontSize: 18),
+            _buildText('Jitta Score: ${stock.jittaScore}'),
+            _buildText('Rank: ${stock.rank}'),
+            _buildText('Latest Price: \$${stock.latestPrice}'),
+            _buildText('Sector: ${stock.sector.name}'),
+            _buildText('Industry: ${stock.industry}'),
+            const SizedBox(height: 16),
+            const Text(
+              'Stock Price History',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 8),
-            Text(
-              'Rank: ${stock.rank}',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Latest Price: \$${stock.latestPrice}',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Sector: ${stock.sector.name}',
-              style: TextStyle(fontSize: 18),
+            const SizedBox(height: 8),
+            SizedBox(
+              height: 300, // chart height
+              child: StockLineChart(graphs: graphs),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildText(String text, {double fontSize = 18, FontWeight fontWeight = FontWeight.normal}) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Text(
+        text,
+        style: TextStyle(fontSize: fontSize, fontWeight: fontWeight),
       ),
     );
   }
